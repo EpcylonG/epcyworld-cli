8348959d78000a41755c6ec3d9e0dc212b25e884
Raul Lopez <raullopez031@gmail.com>
2022-05-23 09:12:08 +0100

    WIP header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Menu from "../Menu";
 import Icon from "../Icon";
+import image from "../../assets/imgs/Logo.png";
 import "./Header.scss";
 
 
@@ -8,6 +9,7 @@ function Header() {
 
     return(
         <div className="header">
+            <img src={image} alt="Icon"/>
             <Menu></Menu>
             <Icon></Icon>
         </div>
--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
 import image from "../../assets/imgs/default_profile.png";
 import "./Icon.scss";
 
 function Icon(){
 
+    const ref = useRef(null);
+
+    // const [profilePicture, setProfilePicture ]= useState(image)
+
+    function handleClick(){ ref.current.click(); }
+
+    function handleAccept(){
+        console.log(ref.current.value);
+        //! CROP AND UPLOAD
+
+    }
+
     return(
-        <div className="profile-img">
+        <div className="profile-img" onClick={handleClick}>
             <img src={image} alt="Icon"/>
+            <input type="file" ref={ref} style={{display: 'none'}} accept=".png, .jpg, .jpeg" onChange={handleAccept}/>
         </div>
     );
 
